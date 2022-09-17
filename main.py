8440ce6a4b90bc01eb67a9e07307ce1b5637c267
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2022-09-17 14:38:19 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/bin/python3
+if (__name__=='__main__'):
+    from time import sleep
+    from sys import stdout
+    with open("text.txt", "r", encoding = 'utf-8') as f:
+        for line in f.readlines():
+            for character in [char for char in line]:
+                print(character, end="")
+                stdout.flush()
+                sleep(0.1)
+            sleep(0.7)
